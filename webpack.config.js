2af1e3f273bab84c53be5bbbe971b5bb53e13f01
webInRoad <1040233545@qq.com>
2021-07-04 19:47:25 +0800

    loader 基本结构

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [{ test: /\.js$/, use: './loaders/replaceLoader.js' }],
+  },
+};
